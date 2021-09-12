073223a2c2fef1de0fac67c55dc5dc87ee25976d
Kourosh <kooroshsajadi@gmail.com>
2021-09-12 21:18:26 +0430

    Configured the routings

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
-import { AppComponent } from './app.component';
+import { HomePageComponent } from './home-page/home-page.component';
 
 const routes: Routes = [
-  { path: '', component: AppComponent},
-  { path: 'login', component: LoginComponent}
+  { path: '', component: HomePageComponent},
+  { path: 'login', component: LoginComponent},
+  // { path: '', redirectTo: '/dashboard', pathMatch: 'full' }
 ];
 
 @NgModule({
--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomePageComponent implements OnInit {
 
+  title = 'iaun-project';
   constructor() { }
 
   ngOnInit(): void {
